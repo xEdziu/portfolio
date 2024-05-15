2af9213cfe30a18a5535e2547158d7ae35af7b83
Adrian Goral <adrian.goral@gmail.com>
2024-05-15 23:53:04 +0200

    Cleanup of css files and added about.html

---
+++ scripts/navbar.js
@@ -0,0 +1,8 @@
+export function toggleMenu() {
+    var x = document.getElementById("nav-menu");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
